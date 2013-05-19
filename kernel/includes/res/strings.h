5f3d0e243b792eddbc2b7f0e9ad76e1e23780444
Corwin Mcknight <lioneltabre@gmail.com>
2013-05-19 14:05:33 -0700

    Started transitioning static strings to kernel/includes/res/strings.h Modified main.c to accept res_strings

---
+++ kernel/includes/res/strings.h
@@ -0,0 +1,7 @@
+#ifndef H_ELEMENTOSRESOURCES_STRINGS
+#define H_ELEMENTOSRESOURCES_STRINGS
+//Names
+#define RES_VERSION_S "0.0.0.1"
+#define RES_SOURCE_S "git@elementOS.github.com"
+#define RES_STARTMESSAGE_S "Starting elementOS v."
+#endif
