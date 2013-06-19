28fd34f7b3adb77a1665c625f0a8c0b85c36e9c3
Corwin Mcknight <lioneltabre@gmail.com>
2013-06-18 19:14:47 -0700

    Fixed errors, changed panic in main to halt

--- kernel/error.c
+++ kernel/error.c
@@ -1,31 +1,7 @@
 #include <textmode.h>
+volatile unsigned char *videoram = (unsigned char *)0xB8000; //For pip
 //#include <error.h>
-/**
-Called when something errors, but it is NOT recoverable.
-**/
-void panic(char* reason)
-{
-	log("!!!!",0x0C,"Kernel Panic! Given reason:"); tm_print(reason); tm_print(".\n");
-	tm_print("DEBUG INFO:\n")
-	log("!!!!",0x0C,"Debug info:\n");
-	log("!!!!",0x0C,"Registers: Failed to retrieve information\n");
-	halt("kernel panic");
-}
-/**
-Called when something errors, but it is recoverable.
-**/
-void oops()
-{
-	log("OOPS",0x0C,"Kernel "); tm_print(reason); tm_print("!\n");
-}
-/**
-Called when something unexpected happens.
-**/
-void woah(char* reason)
-{
-	//tm_print("WOAH! "); tm_print(reason); tm_print("!\n"); 
-	log("WOAH",0x0C,"Kernel "); tm_print(reason); tm_print("!\n");
-}
+
 /**
 Stops the machine
 **/
@@ -35,8 +11,39 @@ void halt(char* reason)
 	log("HALT",0x0C,"Kernel sent SIG_HALT because "); tm_print(reason); tm_print(".\n");
 	log("HALT",0x0C,"Halting...");
 	asm("cli");
+	videoram[0]='!';
 	while(1)
 	{
 		asm("hlt");
+		videoram[2]='~'; // If it can't halt, then display a ~ after the !
 	}
 }
+
+/**
+Called when something errors, but it is NOT recoverable.
+**/
+void panic(char* reason)
+{
+	log("PANIC",0x0C,"Kernel Panic! Given reason:"); tm_print(reason); tm_print(".\n");
+	log("PANIC",0x0C,"Debug info:\n");
+	log("PANIC",0x0C,"Registers: Failed to retrieve information\n");
+	halt("kernel panic");
+}
+
+/**
+Called when something errors, but it is recoverable.
+**/
+void oops(char* reason)
+{
+	log("OOPS",0x0C,"Kernel "); tm_print(reason); tm_print("!\n");
+}
+
+/**
+Called when something unexpected happens.
+**/
+void woah(char* reason)
+{
+	//tm_print("WOAH! "); tm_print(reason); tm_print("!\n"); 
+	log("WOAH",0x0C,"Kernel "); tm_print(reason); tm_print("!\n");
+}
+
