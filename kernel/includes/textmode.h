38b893761d3e951679d1bd32180f2265418d5ad2
Corwin Mcknight <lioneltabre@gmail.com>
2013-06-20 14:03:34 -0700

    Added colors to printf (and used them)
    Version updated, renamed "Best Blue Beluga", can now set attribute byte
    from outside of textmode.

--- kernel/includes/textmode.h
+++ kernel/includes/textmode.h
@@ -6,7 +6,7 @@ void tm_putch_at(char ,int x,int y);
 void tm_clear();
 void tm_print(const char *c);
 void tm_print_at(const char *c,int x,int y);
-void setAttribute(uint8_t color,int x, int y);
+void tm_setAttribute(uint8_t color);
 void move_cursor();
 void log(const char *type,uint8_t color,const char *c);
 extern char cursor_x;
