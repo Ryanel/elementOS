4ada787824d39aff886a8c33cfe714564b2b4d07
Corwin Mcknight <lioneltabre@gmail.com>
2013-08-22 14:09:36 -0700

    Update low.c
    new memset and memcpy trial

--- kernel/low.c
+++ kernel/low.c
@@ -18,6 +18,29 @@ inline void *memset(void *dest, char val, size_t count)
     for( ; count != 0; count--) *temp++ = val;
     return dest;
 }
+void memset_64(void *dest, uint8 sval, uint64 count)
+{
+  uint64 val = (sval & 0xFF); // create a 64-bit version of 'sval'
+  val |= ((val << 8) & 0xFF00);
+  val |= ((val << 16) & 0xFFFF0000);
+  val |= ((val << 32) & 0xFFFFFFFF00000000);
+    
+  if(!count){return;} // nothing to copy?
+  while(count >= 8){ *(uint64*)dest = (uint64)val; memset_transfers_64++; dest += 8; count -= 8; }
+  while(count >= 4){ *(uint32*)dest = (uint32)val; memset_transfers_32++; dest += 4; count -= 4; }
+  while(count >= 2){ *(uint16*)dest = (uint16)val; memset_transfers_16++; dest += 2; count -= 2; }
+  while(count >= 1){ *(uint8*)dest = (uint8)val; memset_transfers_8++; dest += 1; count -= 1; }
+  return; 
+}
+void memcpy_64(void *dest, void *src, uint64 count)
+{
+  if(!count){return;} // nothing to copy?
+  while(count >= 8){ *(uint64*)dest = *(uint64*)src; memcpy_transfers_64++; dest += 8; src += 8; count -= 8; }
+  while(count >= 4){ *(uint32*)dest = *(uint32*)src; memcpy_transfers_32++; dest += 4; src += 4; count -= 4; }
+  while(count >= 2){ *(uint16*)dest = *(uint16*)src; memcpy_transfers_16++; dest += 2; src += 2; count -= 2; }
+  while(count >= 1){ *(uint8*)dest = *(uint8*)src; memcpy_transfers_8++; dest += 1; src += 1; count -= 1; }
+  return;
+}
 inline int strcmp (char *str1, char *str2)
 {
     while (*str1 != 0 && *str2 != 0)
