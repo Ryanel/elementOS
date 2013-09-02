51dcc1433f267c43e7c03e7f598a480cf7d02715
Corwin McKnight <lioneltabre@gmail.com>
2013-09-02 13:09:27 -0700

    Got text printing working

--- kernel-asm/x86/boot.s
+++ kernel-asm/x86/boot.s
@@ -4,46 +4,75 @@
 ; By (as in edited by) Corwin Mcknight -- Credit to James Molloy (jamesmolloy.co.uk)
 ;
 
-MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
-MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
-;MBOOT_VIDEO_MODE    equ 1<<2    ; Set Video Mode
-MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
-; NOTE: We do not use MBOOT_AOUT_KLUDGE. It means that GRUB does not
-; pass us a symbol table.
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO ;| MBOOT_VIDEO_MODE
+MBOOT_PAGE_ALIGN    equ 1<<0
+MBOOT_MEM_INFO      equ 1<<1
+MBOOT_HEADER_MAGIC  equ 0x1BADB002
+MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
 MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
+[BITS 32]
 
-[BITS 32]                       ; All instructions should be 32-bit.
-
-[GLOBAL mboot]                  ; Make 'mboot' accessible from C.
-[EXTERN code]                   ; Start of the '.text' section.
-[EXTERN bss]                    ; Start of the .bss section.
-[EXTERN end]                    ; End of the last loadable section.
+[GLOBAL mboot]
+[EXTERN code]
+[EXTERN bss]
+[EXTERN end]
 section .__mbHeader
 align 0x4
 mboot:
-  dd  MBOOT_HEADER_MAGIC        ; GRUB will search for this value on each
-                                ; 4-byte boundary in your kernel file
-  dd  MBOOT_HEADER_FLAGS        ; How GRUB should load your file / settings
-  dd  MBOOT_CHECKSUM            ; To ensure that the above values are correct
-   
-  dd  mboot                     ; Location of this descriptor
-  dd  code                      ; Start of kernel '.text' (code) section.
-  dd  bss                       ; End of kernel '.data' section.
-  dd  end                       ; End of kernel.
-  dd  start                     ; Kernel entry point (initial EIP).
+	dd  MBOOT_HEADER_MAGIC
+	dd  MBOOT_HEADER_FLAGS
+	dd  MBOOT_CHECKSUM
+
+	dd  mboot
+	dd  code
+	dd  bss
+	dd  end
+	dd  start
+
 section .text
-[GLOBAL start]                  ; Kernel entry point.
 
+dochar:
+	call cprint
+printstring:
+	mov eax, [esi]
+	lea esi, [esi+1]
+	cmp al, 0
+	jne dochar
+	add byte [ypos], 1
+	mov byte [xpos], 0
+	ret
 
+cprint:
+	mov ah, 0x0F ;Color
+	mov ecx, eax
+	movzx eax, byte [ypos]
+	mov edx, 160
+	mul edx
+	movzx ebx, byte [xpos]
+	shl ebx, 1
+
+	mov edi, 0xb8000
+	add edi, eax
+	add edi, ebx
+
+	mov eax, ecx
+	mov word [ds:edi], ax
+	add byte[xpos], 1
+	ret
+
+[GLOBAL start]
 start:
-  cli
-  mov eax, 0xB8000
-  mov bx, "!"
-  mov [eax],bx
+	cli
+	mov esi, startingmsg
+	call printstring
+
+	mov esi, asmtext
+	call printstring
+
 halt:
-  hlt
-  jmp halt                       ; Enter an infinite loop, to stop the processor
-                              ; executing whatever rubbish is in the memory
-                              ; after our kernel!
+	hlt
+	jmp halt
+startingmsg: db "elementOS (asm) started", 0
+asmtext: db "Assembly text working!", 0
+xpos db 0
+ypos db 0
